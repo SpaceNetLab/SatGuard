ad8574850f7031fd1a0aa0632e6543b1d67ef4ce
lijihao88 <65279212+lijihao88@users.noreply.github.com>
2024-02-18 18:57:28 +0800

    Update recovery.sh

--- handover_script /recovery.sh
+++ handover_script /recovery.sh
@@ -1,5 +1,5 @@
 python3 handover1to2_before.py
-sshpass -p admin ssh 240b::2 'sh recovery_route_update.sh'&
-sshpass -p admin ssh 240b::4 'sh recovery_route_update.sh'
+sshpass -p admin ssh 240b::2 'sh sat_recovery_route_update.sh'&
+sshpass -p admin ssh 240b::4 'sh gs_recovery_route_update.sh'
 sleep 0.1
 python3 handover1to2_after.py
