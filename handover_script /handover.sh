6ecd888f3e21c73121104740fbec2b4d73dc898b
lijihao88 <65279212+lijihao88@users.noreply.github.com>
2024-02-18 18:48:53 +0800

    Update handover.sh

--- handover_script /handover.sh
+++ handover_script /handover.sh
@@ -1,1 +1,7 @@
+python3 handover2to1_before.py
+sshpass -p admin ssh 240b::4 'sh handover_route_update.sh'&
+sshpass -p admin ssh 240b::2 'sh handover_route_update.sh'
+sleep 0.1
+python3 handover2to1_after.py
+
 
